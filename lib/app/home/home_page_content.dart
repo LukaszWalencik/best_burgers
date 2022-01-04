fe9bc68ca57cc0c1e6c54138ab3c726f34dd7e74
LukaszWalencik <95505066+LukaszWalencik@users.noreply.github.com>
2022-01-04 21:21:07 +0100

    Add functionaly bottomnavigationbar

--- lib/app/home/home_page_content.dart
+++ lib/app/home/home_page_content.dart
@@ -1,13 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({
+class HomePage extends StatefulWidget {
+  HomePage({
     Key? key,
     required this.user,
   }) : super(key: key);
 
   final User user;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var bottomindex = 2;
 
   @override
   Widget build(BuildContext context) {
@@ -16,16 +23,30 @@ class HomePage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Jestes zalogowany '),
+            Text('Jestes zalogowany ${widget.user.email}'),
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 FirebaseAuth.instance.signOut();
               },
               child: Text('Wyloguj'),
-            )
+            ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: bottomindex,
+        onTap: (newbottomindex) {
+          setState(() {
+            bottomindex = newbottomindex;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.reviews), label: 'Opinie'),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Dodaj opinie'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), label: 'Moje konto'),
+        ],
       ),
     );
   }

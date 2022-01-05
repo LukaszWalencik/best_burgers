6d4454e3ca3730fff59cefa0f621b057f2279486
LukaszWalencik <95505066+LukaszWalencik@users.noreply.github.com>
2022-01-05 16:27:45 +0100

    Add Cloud Firestor to opinion_page_content

--- lib/app/home/bottom_bar_pages/add_opinion_page_content.dart
+++ lib/app/home/bottom_bar_pages/add_opinion_page_content.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-class AddOpinionPage extends StatelessWidget {
+class AddOpinionPage extends StatefulWidget {
   const AddOpinionPage({
     Key? key,
   }) : super(key: key);
 
+  @override
+  State<AddOpinionPage> createState() => _AddOpinionPageState();
+}
+
+class _AddOpinionPageState extends State<AddOpinionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/app/home/bottom_bar_pages/opinion_page_content.dart
+++ lib/app/home/bottom_bar_pages/opinion_page_content.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class OpinionPage extends StatelessWidget {
@@ -12,9 +13,37 @@ class OpinionPage extends StatelessWidget {
         centerTitle: true,
         title: Text('Ekran pierwszy'),
       ),
-      body: Center(
-        child: Text('Ekran pierwszy'),
-      ),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream: FirebaseFirestore.instance.collection('places').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text("Loading");
+            }
+            final documents = snapshot.data!.docs;
+            return ListView(children: [
+              for (final document in documents) ...[
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text(document['name']),
+                          Text(document['foodname']),
+                        ],
+                      ),
+                      Text(document['rating'].toString()),
+                    ],
+                  ),
+                )
+              ]
+            ]);
+          }),
     );
   }
 }

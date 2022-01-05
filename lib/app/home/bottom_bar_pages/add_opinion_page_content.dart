6f2b74e4c85e2ed73483c13dfc7fc1246e85de57
LukaszWalencik <95505066+LukaszWalencik@users.noreply.github.com>
2022-01-05 17:08:32 +0100

    Change AddOpinionPage to StatefullWidget Add slider TextFields for restaurant name and food name

--- lib/app/home/bottom_bar_pages/add_opinion_page_content.dart
+++ lib/app/home/bottom_bar_pages/add_opinion_page_content.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class AddOpinionPage extends StatefulWidget {
@@ -10,6 +11,9 @@ class AddOpinionPage extends StatefulWidget {
 }
 
 class _AddOpinionPageState extends State<AddOpinionPage> {
+  var placeName = '';
+  var foodName = '';
+  var rating = 3.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +21,53 @@ class _AddOpinionPageState extends State<AddOpinionPage> {
         centerTitle: true,
         title: Text('Ekran drugi'),
       ),
-      body: Center(child: Text('Ekran drugi')),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(hintText: 'Nazwa restauracji'),
+                onChanged: (newplaceName) {
+                  setState(() {
+                    placeName = newplaceName;
+                  });
+                },
+              ),
+              SizedBox(height: 20),
+              TextField(
+                onChanged: (newfoodName) {
+                  setState(() {
+                    foodName = newfoodName;
+                  });
+                },
+                decoration: InputDecoration(hintText: 'Nazwa dania'),
+              ),
+              SizedBox(height: 10),
+              Slider(
+                  value: rating,
+                  min: 1.0,
+                  max: 6.0,
+                  divisions: 5,
+                  label: rating.toString(),
+                  onChanged: (newrating) {
+                    setState(() {
+                      rating = newrating;
+                    });
+                  }),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  FirebaseFirestore.instance.collection('places').add(
+                    {'name': placeName, 'foodname': foodName, 'rating': rating},
+                  );
+                },
+                child: Text('Dodaj opinie'),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
